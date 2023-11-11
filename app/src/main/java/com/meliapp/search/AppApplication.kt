0f1148c01e11f357eb3b1ae01502a34e2ac373b0
Cesar Yepes <cyepes970@gmail.com>
2023-11-11 10:15:22 -0500

    start koin on app init

---
+++ app/src/main/java/com/meliapp/search/AppApplication.kt
@@ -0,0 +1,21 @@
+package com.meliapp.search
+
+import android.app.Application
+import com.meliapp.search.data.di.getDataModule
+import com.meliapp.search.domain.di.getDomainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.startKoin
+
+class AppApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@AppApplication)
+            modules(
+                getDataModule(),
+                getDomainModule(),
+            )
+        }
+    }
+}
